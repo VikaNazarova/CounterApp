ec9a0ec4d1f0843dedd7ca37c97dff52eb4f2c8b
VikaNazarova <33890177+VikaNazarova@users.noreply.github.com>
2020-08-27 15:44:39 +0400

    finished task 1.3

--- src/components/BoundedCounter.js
+++ src/components/BoundedCounter.js
@@ -1,36 +1,51 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class BoundedCounter extends React.Component {
   state = {
     count: 0,
-    disabled: false
+    disabledPlus: false,
+    disabledMinus: false
   };
   add = (state, props) => {
     this.setState((state, props) => ({
-      count: state.count + 1,
-      disabled: (this.props.max <= this.state.count) ? true : false
+      disabledPlus:
+        (this.props.max = this.props.max - 1) <= this.state.count
+          ? true
+          : false,
+      count: state.count + 1
     }));
   };
   substract = (state, props) => {
     this.setState((state, props) => ({
-      count: state.count - 1,
-      disabled: (this.props.min >= this.state.count) ? true : false
+      disabledMinus:
+        (this.props.min = this.props.min + 1) >= this.state.count
+          ? true
+          : false,
+      count: state.count - 1
     }));
   };
-  
+
   render() {
     return (
       <div>
         <h2>Задача 1.3 – Counter with bounds</h2>
         <p>Button was clicked</p>
-        <button disabled={this.state.disabled} onClick={this.substract}>-</button>
-        <span style={{margin: '0 1rem'}}>
-            {this.state.count} times
-        </span>
-        <button disabled={this.state.disabled} onClick={this.add}>+</button>
+        <button disabled={this.state.disabledMinus} onClick={this.substract}>
+          -
+        </button>
+        <span style={{ margin: "0 1rem" }}>{this.state.count} times</span>
+        <button disabled={this.state.disabledPlus} onClick={this.add}>
+          +
+        </button>
       </div>
     );
   }
 }
+
+// BoundedCounter.propTypes = {
+//   max: PropTypes.number,
+//   min: PropTypes.number
+// };
 
 export default BoundedCounter;
