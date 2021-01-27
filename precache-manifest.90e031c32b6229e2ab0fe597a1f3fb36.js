d204dcc0a01c2c3780413935754ce02a52a8abcd
VikaNazarova <vikanaz98@gmail.com>
2021-01-27 22:57:04 +0400

    Updates

---
+++ precache-manifest.90e031c32b6229e2ab0fe597a1f3fb36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1b21a50a0bfe8b094f180214b450a1",
+    "url": "/CounterApp/index.html"
+  },
+  {
+    "revision": "41f4825a6d7fc9a5ffbf",
+    "url": "/CounterApp/static/css/main.0d3efd17.chunk.css"
+  },
+  {
+    "revision": "31121b01079c3de5a9f2",
+    "url": "/CounterApp/static/js/2.c912b2cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CounterApp/static/js/2.c912b2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f4825a6d7fc9a5ffbf",
+    "url": "/CounterApp/static/js/main.7f862e84.chunk.js"
+  },
+  {
+    "revision": "bf07dff00b926d99e8e5",
+    "url": "/CounterApp/static/js/runtime-main.4659a493.js"
+  }
+]);
